c3acc7e392dd870b9ebf20748bc94badfe21aa40
RGuzzy96 <ryanguzzwell@gmail.com>
2024-02-12 17:14:06 -0400

    refined theme

--- components/blog.tsx
+++ components/blog.tsx
@@ -1,7 +1,7 @@
 export default function BlogComponent() {
     return (
         <div className="flex flex-col gap-y-6 text-gray-900 dark:text-stone-300">
-            <h1 className="text-xl font-semibold">my blog</h1>
+            <h1 className="text-3xl font-semibold">my blog</h1>
         </div>
     )
 }
